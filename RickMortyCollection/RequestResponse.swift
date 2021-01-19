e95d03a9a1c2ffc3a265f6a1b5d92cb9c6a42ab0
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-01-19 21:04:37 +0100

    Separate request structs in separate file and class

---
+++ RickMortyCollection/RequestResponse.swift
@@ -0,0 +1,39 @@
+//
+//  RequestResponse.swift
+//  RickMortyCollection
+//
+//  Created by A4-iMAC01 on 19/01/2021.
+//
+
+import Foundation
+
+class RequestResponse {
+    struct CharacterListResponse:Decodable {
+        let info:CharacterListResponseInfo
+        let results:[CharacterListResponseResult]
+    }
+    struct  CharacterListResponseInfo:Decodable{
+        let count:Int
+        let pages:Int
+        let next:String?
+        let prev:String?
+    }
+    struct  CharacterListResponseResult:Decodable {
+        let id:Int
+        let name:String
+        let status:String
+        let species:String
+        let type:String
+        let gender:String
+        let origin:InfoReference
+        let location:InfoReference
+        let image:String
+        let episode:[String]
+        let url:String
+        let created:String
+    }
+    struct InfoReference:Decodable {
+        let name:String
+        let url:String?
+    }
+}
